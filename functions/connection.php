996fa63babb256b5cd538fe341a844ed6dd4484d
Moisés Aguilar <monchito007@gmail.com>
2020-06-17 13:25:44 +0200

    Update connection.php

--- functions/connection.php
+++ functions/connection.php
@@ -3,7 +3,7 @@
 mysql_connect("localhost","root","");
 mysql_select_db("test");
 //Conexion Servidor MBI
-//mysql_connect("localhost","s05a5ae8","iddqdidkfa");
+//mysql_connect("localhost","s05a5ae8","password");
 //mysql_select_db("s05a5ae8_projecte_daw");
 
 ?>
